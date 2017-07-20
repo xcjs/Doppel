0c9e60624cb87245dd0419235f176601a0e4ecb2
Zackary Lowery <zlowery@xcjs.com>
2017-07-20 18:59:20 -0400

    Added a unit test project for Doppel.Providers.FileSystem.

---
+++ Doppel.Providers.FileSystem.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Doppel.Providers.FileSystem.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
